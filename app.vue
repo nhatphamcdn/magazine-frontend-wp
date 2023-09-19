9fa87b4f34d3f3ec10fefdd72b828695ee6f54c3
Nhat Pham <nhatphamcdn@gmail.com>
2023-09-19 21:56:35 +0700

    feat: add layout sample

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <NuxtLoadingIndicator color="repeating-linear-gradient(to right,var(--c-primary) 0%,var(--c-primary-active) 100%)" />
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
